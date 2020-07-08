3fb3a63f762cd5dacc0ecb14d2dfb6fe34f3768a
Doupro <doupros@live.com>
2020-07-08 23:41:17 +0100

    Fixed Grab System

---
+++ Assets/Gun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float speed = 40;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public Transform gunTransform;
+
+
+    public void Fire()
+    {
+        GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+        Vector3 temp = new Vector3(10, 0, 0);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        audioSource.PlayOneShot(audioClip);
+        Destroy(spawnedBullet, 2);
+    }
+}
---
+++ Assets/Scripts/LocoMotionController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocoMotionController : MonoBehaviour
+{
+
+    public XRController leftTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.2f;
+
+    public bool EnableLeftTeleport { get; set; } = true;
+    public bool EnableRightTeleport { get; set; } = true;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightTeleportRay)
+        {
+            rightTeleportRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(rightTeleportRay));
+        }
+        if (leftTeleportRay)
+        {
+            leftTeleportRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(leftTeleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller) 
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActived,activationThreshold);
+
+        return isActived;
+    }
+
+}
