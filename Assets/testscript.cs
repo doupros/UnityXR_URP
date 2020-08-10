d07a4b53b5b5ab5f42ce37a6d220bec17c83f103
Doupro <doupros@live.com>
2020-08-10 22:59:57 +0100

    Test script for testing get imformation from instantated prefabs teleport system disabled

---
+++ Assets/testscript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    public Node nude;
+    public GameObject nodePrefab;
+    public string name;
+    public float number;
+    private void Start()
+    {
+        GameObject spawnedNode = Instantiate(nodePrefab, transform);
+        number = spawnedNode.GetComponent<Node>().number;
+        name = spawnedNode.GetComponent<Node>().name;
+    }
+
+    public void test1()
+    {
+        Debug.Log("testing interact");
+        Debug.Log(name);
+        Debug.Log(number);
+
+    }
+}
